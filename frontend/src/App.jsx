6243754220f947d356baed05f4aa555d9c0c42e4
João Rocha <78546581+john0x01@users.noreply.github.com>
2022-12-18 22:22:41 -0300

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { FaUsers } from 'react-icons/fa'
+
+import React from 'react'
+import Sidebar from './components/sidebar/Sidebar'
+
+import './App.css'
+
+
+const App = () => {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const openSidebar = () => {
+    setSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setSidebarOpen(false)
+  }
+
+  return (
+    <div className='container'>
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+    </div>
+  )
+}
+
+export default App
