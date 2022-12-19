46389b89c481ded226e356ca899aa178ce433705
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2022-12-19 01:34:24 -0300

    Downgrade from React 18 to 17 (react-vis deprecated)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom'
 
 import App from './App'
 
 import './index.css'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
